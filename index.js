b5f7c01e4b34bdea27dc3c57b21a636f2e9fae54
Dawn LeBlanc <dawnleblanc11@gmail.com>
2022-01-13 17:24:35 -0500

    add to AWS

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
-  var express = require('express')
-  var multer  = require('multer')
+  const express = require('express')
+  const multer  = require('multer')
+  const { uploadFile } = require('./s3')
+
   var port = 3001;
   
   var app = express()
@@ -13,19 +15,20 @@
         cb(null, file.originalname)
       }
   })
-  var upload = multer({ storage: storage })
+  const upload = multer({ storage: storage })
   
   
   app.use(express.static(__dirname + '/public'));
   app.use('/uploads', express.static('uploads'));
   
-  app.post('/profile-upload-single', upload.single('profile-file'), function (req, res, next) {
+  app.post('/profile-upload-single', upload.single('profile-file'), (req,res) => {
     // req.file is the `profile-file` file
     // req.body will hold the other blog post fields
     console.log(JSON.stringify(req.file))
     var response = '<a href="/">Home</a><br>'
     response += "Files uploaded successfully.<br>"
     response += `<img src="${req.file.path}" /><br>`
+    uploadFile(req.file.path)
     return res.send(response)
   })
   
---
+++ s3.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+const fs = require('fs')
+const S3 = require('aws-sdk/clients/s3')
+
+
+const bucketName = process.env.AWS_BUCKET_NAME
+const region = process.env.AWS_BUCKET_REGION
+const accessKeyId = process.env.AWS_ACCESS_KEY
+const secretAccessKey = process.env.AWS_SECRET_KEY
+
+//uploads a file to AWS s3
+const s3 = new S3 ({
+    region,
+    accessKeyId,
+    secretAccessKey
+})
+
+
+function uploadFile(file) {
+ const fileStream= fs.createReadStream(file)
+ console.log(fileStream)
+ const uploadParams = {
+     Bucket: bucketName,
+     Body: fileStream,
+     Key: "Airplane.jpg"
+ }
+    console.log('preparing to send to AWS')
+    console.log(uploadParams)
+    return s3.upload(uploadParams).promise()
+}
+exports.uploadFile = uploadFile
+
+
+
+// downloads a file from AWS s3
